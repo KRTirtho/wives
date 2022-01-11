7649f71fd4c8c4c72aab8c80ad746de35cadc2ea
LarsB24 <larsb24@outlook.com>
2022-01-11 12:21:15 +0100

    Improve the tabs system

--- lib/views/terminal_view.dart
+++ lib/views/terminal_view.dart
@@ -21,19 +21,35 @@ PseudoTerminal get _pty => PseudoTerminal.start(
 class _TheTerminalViewState extends State<TheTerminalView> {
   int index = 0;
   List<Terminal> tabs = [Constants.terminal(_pty)];
+  List<FocusNode> focusNodes = [FocusNode()];
+
+  @override
+  void initState() {
+    super.initState();
+
+    focusNodes[index].requestFocus();
+  }
+
   @override
   Widget build(BuildContext context) {
     return TabView(
+      shortcutsEnabled: true,
       currentIndex: index,
-      onChanged: (newIndex) => setState(() {
-        index = newIndex;
-        tabs[newIndex].notifyListeners();
-      }),
-      onNewPressed: () {
+      onChanged: (newIndex) async {
+        setState(() {
+          index = newIndex;
+        });
+        await Future.delayed(const Duration(milliseconds: 300));
+        focusNodes[newIndex].requestFocus();
+      },
+      onNewPressed: () async {
         setState(() {
           tabs.add(Constants.terminal(_pty));
+          focusNodes.add(FocusNode());
           index = tabs.length - 1;
         });
+        await Future.delayed(const Duration(milliseconds: 300));
+        focusNodes[index].requestFocus();
       },
       tabs: List.generate(
         tabs.length,
@@ -53,7 +69,10 @@ class _TheTerminalViewState extends State<TheTerminalView> {
         ),
       ),
       bodies: tabs
-          .map((Terminal terminal) => TerminalView(terminal: terminal))
+          .map((Terminal terminal) => TerminalView(
+                terminal: terminal,
+                focusNode: focusNodes[index],
+              ))
           .toList(),
     );
   }
